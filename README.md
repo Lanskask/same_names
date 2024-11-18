be9451e0e92b4540c4577aaa3ec56d80f263aa6d
Lanskask <grey.wooden.tractor@gmail.com>
2024-11-18 02:03:20 -0300

    First commit Added cards that fit and filtering in them

---
+++ README.md
@@ -0,0 +1,4 @@
+# Same Names
+
+Find different forms of the same name in different languages 
+
---
+++ name_variants.js
@@ -0,0 +1,13 @@
+export const namesDatabase = {
+    "John": ["John", "Jean", "Juan", "Giovanni", "Johann", "Иван", "Ιωάννης", "يحيى"],
+    "Mary": ["Mary", "Marie", "María", "Maria", "Мария", "Μαρία", "مريم"],
+    "Michael": ["Michael", "Michel", "Miguel", "Michele", "Михаил", "Μιχαήλ", "ميخائيل"],
+    "Elizabeth": ["Elizabeth", "Élisabeth", "Isabel", "Elisabetta", "Елизавета", "Ελισάβετ", "إليزابيث"],
+    "William": ["William", "Guillaume", "Guillermo", "Guglielmo", "Вильгельм", "Ουίλιαμ", "وليام"],
+    "Catherine": ["Catherine", "Catherine", "Catalina", "Caterina", "Екатерина", "Αικατερίνη", "كاثرين"],
+    "Alexander": ["Alexander", "Alexandre", "Alejandro", "Alessandro", "Александр", "Αλέξανδρος", "إسكندر"],
+    "Peter": ["Peter", "Pierre", "Pedro", "Pietro", "Пётр", "Πέτρος", "بطرس"],
+    "Anna": ["Anna", "Anne", "Ana", "Анна", "Άννα", "آنا"],
+    "James": ["James", "Jacques", "Santiago", "Giacomo", "Яков", "Ιάκωβος", "يعقوب"]
+};
+
