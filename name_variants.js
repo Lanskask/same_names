2a41dcb82e464b5467cd33ccfc9d9e8fa56b998a
Lanskask <grey.wooden.tractor@gmail.com>
2024-11-18 02:34:11 -0300

    Added more names

--- name_variants.js
+++ name_variants.js
@@ -1,12 +1,114 @@
 export const namesDatabase = {
-    "John": { "variants": ["John (English)", "Jean (French)", "Juan (Spanish)", "Giovanni (Italian)", "Johann (German)", "Иван [Ivan] (Russian)", "Ιωάννης [Ioannis] (Greek)", "يحيى [Yahya] (Arabic)"], "origin": "english" },
-    "Mary": { "variants": ["Mary (English)", "Marie (French)", "María (Spanish)", "Maria (Italian)", "Мария [Mariya] (Russian)", "Μαρία [Maria] (Greek)", "مريم [Maryam] (Arabic)"], "origin": "hebrew" },
-    "Michael": { "variants": ["Michael (English)", "Michel (French)", "Miguel (Spanish)", "Michele (Italian)", "Михаил [Mikhail] (Russian)", "Μιχαήλ [Mikhail] (Greek)", "ميخائيل [Mikhail] (Arabic)"], "origin": "hebrew" },
-    "Elizabeth": { "variants": ["Elizabeth (English)", "Élisabeth (French)", "Isabel (Spanish)", "Elisabetta (Italian)", "Елизавета [Elizaveta] (Russian)", "Ελισάβετ [Elisavet] (Greek)", "إليزابيث [Elizabith] (Arabic)"], "origin": "hebrew" },
-    "William": { "variants": ["William (English)", "Guillaume (French)", "Guillermo (Spanish)", "Guglielmo (Italian)", "Вильгельм [Vilgelm] (Russian)", "Ουίλιαμ [Ouiliam] (Greek)", "وليام [Wiliam] (Arabic)"], "origin": "germanic" },
-    "Catherine": { "variants": ["Catherine (English)", "Catherine (French)", "Catalina (Spanish)", "Caterina (Italian)", "Екатерина [Yekaterina] (Russian)", "Αικατερίνη [Aikaterini] (Greek)", "كاثرين [Kathrin] (Arabic)"], "origin": "greek" },
-    "Alexander": { "variants": ["Alexander (English)", "Alexandre (French)", "Alejandro (Spanish)", "Alessandro (Italian)", "Александр [Aleksandr] (Russian)", "Αλέξανδρος [Alexandros] (Greek)", "إسكندر [Iskandar] (Arabic)"], "origin": "greek" },
-    "Peter": { "variants": ["Peter (English)", "Pierre (French)", "Pedro (Spanish)", "Pietro (Italian)", "Пётр [Pyotr] (Russian)", "Πέτρος [Petros] (Greek)", "بطرس [Butrus] (Arabic)"], "origin": "greek" },
-    "Anna": { "variants": ["Anna (English)", "Anne (French)", "Ana (Spanish)", "Анна [Anna] (Russian)", "Άννα [Anna] (Greek)", "آنا [Ana] (Arabic)"], "origin": "hebrew" },
-    "James": { "variants": ["James (English)", "Jacques (French)", "Santiago (Spanish)", "Giacomo (Italian)", "Яков [Yakov] (Russian)", "Ιάκωβος [Iakovos] (Greek)", "يعقوب [Yaqub] (Arabic)"], "origin": "hebrew" }
+    "John": {
+        "variants": ["John (English)", "Jean (French)", "Juan (Spanish)", "Giovanni (Italian)", "Johann (German)", "Иван [Ivan] (Russian)", "Ιωάννης [Ioannis] (Greek)", "يحيى [Yahya] (Arabic)"],
+        "origin": "english"
+    },
+    "Mary": {
+        "variants": ["Mary (English)", "Marie (French)", "María (Spanish)", "Maria (Italian)", "Мария [Mariya] (Russian)", "Μαρία [Maria] (Greek)", "مريم [Maryam] (Arabic)"],
+        "origin": "hebrew"
+    },
+    "Michael": {
+        "variants": ["Michael (English)", "Michel (French)", "Miguel (Spanish)", "Michele (Italian)", "Михаил [Mikhail] (Russian)", "Μιχαήλ [Mikhail] (Greek)", "ميخائيل [Mikhail] (Arabic)"],
+        "origin": "hebrew"
+    },
+    "Elizabeth": {
+        "variants": ["Elizabeth (English)", "Élisabeth (French)", "Isabel (Spanish)", "Elisabetta (Italian)", "Елизавета [Elizaveta] (Russian)", "Ελισάβετ [Elisavet] (Greek)", "إليزابيث [Elizabith] (Arabic)"],
+        "origin": "hebrew"
+    },
+    "William": {
+        "variants": ["William (English)", "Guillaume (French)", "Guillermo (Spanish)", "Guglielmo (Italian)", "Вильгельм [Vilgelm] (Russian)", "Ουίλιαμ [Ouiliam] (Greek)", "وليام [Wiliam] (Arabic)"],
+        "origin": "germanic"
+    },
+    "Catherine": {
+        "variants": ["Catherine (English)", "Catherine (French)", "Catalina (Spanish)", "Caterina (Italian)", "Екатерина [Yekaterina] (Russian)", "Αικατερίνη [Aikaterini] (Greek)", "كاثرين [Kathrin] (Arabic)"],
+        "origin": "greek"
+    },
+    "Alexander": {
+        "variants": ["Alexander (English)", "Alexandre (French)", "Alejandro (Spanish)", "Alessandro (Italian)", "Александр [Aleksandr] (Russian)", "Αλέξανδρος [Alexandros] (Greek)", "إسكندر [Iskandar] (Arabic)"],
+        "origin": "greek"
+    },
+    "Peter": {
+        "variants": ["Peter (English)", "Pierre (French)", "Pedro (Spanish)", "Pietro (Italian)", "Пётр [Pyotr] (Russian)", "Πέτρος [Petros] (Greek)", "بطرس [Butrus] (Arabic)"],
+        "origin": "greek"
+    },
+    "Anna": {
+        "variants": ["Anna (English)", "Anne (French)", "Ana (Spanish)", "Анна [Anna] (Russian)", "Άννα [Anna] (Greek)", "آنا [Ana] (Arabic)"],
+        "origin": "hebrew"
+    },
+    "James": {
+        "variants": ["James (English)", "Jacques (French)", "Santiago (Spanish)", "Giacomo (Italian)", "Яков [Yakov] (Russian)", "Ιάκωβος [Iakovos] (Greek)", "يعقوب [Yaqub] (Arabic)"],
+        "origin": "hebrew"
+    },
+    "Joseph": {
+        "variants": ["Joseph (English)", "Joseph (French)", "José (Spanish)", "Giuseppe (Italian)", "Иосиф [Iosif] (Russian)", "Ιωσήφ [Iosif] (Greek)", "يوسف [Yusuf] (Arabic)"],
+        "origin": "hebrew"
+    },
+    "David": {
+        "variants": ["David (English)", "David (French)", "David (Spanish)", "Davide (Italian)", "Давид [David] (Russian)", "Δαβίδ [David] (Greek)", "داود [Dawud] (Arabic)"],
+        "origin": "hebrew"
+    },
+    "Sophia": {
+        "variants": ["Sophia (English)", "Sophie (French)", "Sofía (Spanish)", "Sofia (Italian)", "София [Sofiya] (Russian)", "Σοφία [Sophia] (Greek)", "صوفيا [Sophia] (Arabic)"],
+        "origin": "greek"
+    },
+    "Daniel": {
+        "variants": ["Daniel (English)", "Daniel (French)", "Daniel (Spanish)", "Daniele (Italian)", "Даниил [Daniil] (Russian)", "Δανιήλ [Daniil] (Greek)", "دانيال [Daniyal] (Arabic)"],
+        "origin": "hebrew"
+    },
+    "Andrew": {
+        "variants": ["Andrew (English)", "André (French)", "Andrés (Spanish)", "Andrea (Italian)", "Андрей [Andrey] (Russian)", "Ανδρέας [Andreas] (Greek)", "أندرو [Andru] (Arabic)"],
+        "origin": "greek"
+    },
+    "Emma": {
+        "variants": ["Emma (English)", "Emma (French)", "Emma (Spanish)", "Emma (Italian)", "Эмма [Emma] (Russian)", "Έμμα [Emma] (Greek)", "إيما [Emma] (Arabic)"],
+        "origin": "germanic"
+    },
+    "Eleanor": {
+        "variants": ["Eleanor (English)", "Éléonore (French)", "Leonor (Spanish)", "Eleonora (Italian)", "Элеонора [Eleonora] (Russian)", "Ελεονώρα [Eleonora] (Greek)", "إلينور [Eleanor] (Arabic)"],
+        "origin": "old french"
+    },
+    "Thomas": {
+        "variants": ["Thomas (English)", "Thomas (French)", "Tomás (Spanish)", "Tommaso (Italian)", "Фома [Foma] (Russian)", "Θωμάς [Thomas] (Greek)", "توماس [Tomas] (Arabic)"],
+        "origin": "aramaic"
+    },
+    "Henry": {
+        "variants": ["Henry (English)", "Henri (French)", "Enrique (Spanish)", "Enrico (Italian)", "Генри [Genri] (Russian)", "Ερρίκος [Errikos] (Greek)", "هنري [Henry] (Arabic)"],
+        "origin": "germanic"
+    },
+    "George": {
+        "variants": ["George (English)", "Georges (French)", "Jorge (Spanish)", "Giorgio (Italian)", "Георгий [Georgiy] (Russian)", "Γεώργιος [Georgios] (Greek)", "جورج [Jorj] (Arabic)"],
+        "origin": "greek"
+    },
+    "Rebecca": {
+        "variants": ["Rebecca (English)", "Rebecca (French)", "Rebeca (Spanish)", "Rebecca (Italian)", "Ребекка [Rebekka] (Russian)", "Ρεβέκκα [Revekka] (Greek)", "رفقة [Rifqa] (Arabic)"],
+        "origin": "hebrew"
+    },
+    "Margaret": {
+        "variants": ["Margaret (English)", "Marguerite (French)", "Margarita (Spanish)", "Margherita (Italian)", "Маргарита [Margarita] (Russian)", "Μαργαρίτα [Margarita] (Greek)", "مارغريت [Margaret] (Arabic)"],
+        "origin": "greek"
+    },
+    "Edward": {
+        "variants": ["Edward (English)", "Édouard (French)", "Eduardo (Spanish)", "Edoardo (Italian)", "Эдуард [Eduard] (Russian)", "Εδουάρδος [Edouardos] (Greek)", "إدوارد [Edward] (Arabic)"],
+        "origin": "anglo-saxon"
+    },
+    "Christopher": {
+        "variants": ["Christopher (English)", "Christophe (French)", "Cristóbal (Spanish)", "Cristoforo (Italian)", "Кристофер [Kristofer] (Russian)", "Χριστόφορος [Christoforos] (Greek)", "كريستوفر [Kristofer] (Arabic)"],
+        "origin": "greek"
+    },
+    "Samuel": {
+        "variants": ["Samuel (English)", "Samuel (French)", "Samuel (Spanish)", "Samuele (Italian)", "Самуил [Samuil] (Russian)", "Σαμουήλ [Samouil] (Greek)", "صموئيل [Samuel] (Arabic)"],
+        "origin": "hebrew"
+    },
+    "Isabelle": {
+        "variants": ["Isabelle (English)", "Isabelle (French)", "Isabela (Spanish)", "Isabella (Italian)", "Изабелла [Izabella] (Russian)", "Ισαβέλλα [Isavella] (Greek)", "إيزابيلا [Isabella] (Arabic)"],
+        "origin": "hebrew"
+    },
+    "Victoria": {
+        "variants": ["Victoria (English)", "Victoire (French)", "Victoria (Spanish)", "Vittoria (Italian)", "Виктория [Viktoriya] (Russian)", "Βικτωρία [Viktoria] (Greek)", "فيكتوريا [Victoria] (Arabic)"],
+        "origin": "latin"
+    },
+    "Lucas": {
+        "variants": ["Lucas (English)", "Lucas (French)", "Lucas (Spanish)", "Luca (Italian)", "Лукас [Lukas] (Russian)", "Λουκάς [Loukas] (Greek)", "لوكاس [Lukas] (Arabic)"],
+        "origin": "greek"
+    },
 };
